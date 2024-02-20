99aec6c25882a6d59731d20e96506407be0bfbc1
Singh <manasi.singh@capgemini.com>
2024-02-20 21:08:53 +0530

    config_changes

---
+++ src/main/java/com/capgemini/bgvautomation/BgvautomationApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.bgvautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgvautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgvautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/bgvautomation/BgvautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.bgvautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgvautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
